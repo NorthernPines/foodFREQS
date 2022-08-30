29a89b5149a6bf87a4250432f7bb4630d0de8ade
Northern Pines <traviswalborn8@gmail.com>
2022-08-30 14:27:37 -0400

    recipe Routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,28 +14,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/project/:id', async (req, res) => {
-//   try {
-//     const projectData = await Project.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
-
-//     const project = projectData.get({ plain: true });
-
-//     res.render('project', {
-//       ...project,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 // Use withAuth middleware to prevent access to route
 router.get('/:profile', withAuth, async (req, res) => {
   try {
