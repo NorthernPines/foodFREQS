eaee04f181b913ba82606f1886e79c3daa945aa9
wjimh314 <wjimh314@gmail.com>
2022-08-31 08:58:04 -0400

    started and finished most of readME file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,16 +3,16 @@ const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 //dont need most of this
-router.get('/', async (req, res) => {
-  try {
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//router.get('/', async (req, res) => {
+  //try {
+    //res.render('homepage', { 
+      //projects, 
+      //logged_in: req.session.logged_in 
+    //});
+  //} catch (err) {
+    //res.status(500).json(err);
+  //}
+//});
 
 // Use withAuth middleware to prevent access to route
 router.get('/:profile', withAuth, async (req, res) => {
